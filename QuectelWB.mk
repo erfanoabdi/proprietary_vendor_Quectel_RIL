d84f0e3ef38d5e975d9423dda7f9f7624269de4c
Erfan Abdi <erfangplus@gmail.com>
2025-07-01 22:09:52 +0330

    Add makefiles

---
+++ QuectelWB.mk
@@ -0,0 +1,34 @@
+# RILD Related
+PRODUCT_COPY_FILES += \
+    vendor/Quectel_RIL/libril/32/libril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libril.so \
+    vendor/Quectel_RIL/libril/64/libril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libril.so \
+    vendor/Quectel_RIL/libreference-ril/32/libreference-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib/libreference-ril.so \
+    vendor/Quectel_RIL/libreference-ril/64/libreference-ril.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libreference-ril.so
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.5.vendor  \
+    android.hardware.radio.config@1.2.vendor  \
+    android.hardware.secure_element@1.2.vendor
+
+PRODUCT_PACKAGES += \
+    rild \
+    librilutils \
+    ril-quectel.xml
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.telephony.get_imsi_from_sim=true \
+    ro.telephony.default_network=10 \
+    persist.telephony.support.ipv6=1 \
+    persists.telephony.support.ipv4=1 \
+    telephony.lteOnGsmDevice=1 \
+    telephony.lteOnCdmaDevice=0 \
+    vendor.rild.libpath=/vendor/lib64/libril.so \
+    vendor.rild.libargs=-d/dev/ttyUSB2 \
+    ril.function.dataonly=0
