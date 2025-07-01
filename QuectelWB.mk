cd6c38e2c22dbdf92b77ac49002050c7e2b30fe4
Erfan Abdi <erfangplus@gmail.com>
2025-07-01 22:10:12 +0330

    Disable GPS for now

--- QuectelWB.mk
+++ QuectelWB.mk
@@ -44,24 +44,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.rild.libpath=/vendor/lib64/libril.so \
     vendor.rild.libargs=-d/dev/ttyUSB2 \
     ril.function.dataonly=0
-
-# GPS Related
-PRODUCT_PACKAGES += \
-    android.hardware.gnss-V1-ndk.vendor
-
-PRODUCT_PACKAGES += \
-    gnss-quectel.xml
-
-PRODUCT_COPY_FILES += \
-    vendor/Quectel_RIL/libgps/64/android.hardware.gnss-service.example:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.gnss-service.example \
-    vendor/Quectel_RIL/libgps/32/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/gps.default.so \
-    vendor/Quectel_RIL/libgps/64/gps.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/gps.default.so \
-    vendor/Quectel_RIL/libgps/64/gps_cfg.inf:$(TARGET_COPY_OUT_VENDOR)/etc/gps_cfg.inf \
-    vendor/Quectel_RIL/libgps/64/gnss-default.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/gnss-quectel.rc \
-    frameworks/native/data/etc/android.hardware.location.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.kernel.android.gps=ttyUSB1 \
-    ro.kernel.android.gps.speed=115200 \
-    ro.kernel.android.gps.max_rate=1
